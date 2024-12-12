5f3c3a4788be4cee5f3032b674d180a6c12cc63a
1U23CA056LOKESH <139735704+1U23CA056LOKESH@users.noreply.github.com>
2024-12-12 10:09:05 +0000

    submit

--- test.java
+++ test.java
@@ -4,13 +4,13 @@ import java.util.Scanner;
 
 class test {
   public static void main(String[] args) {
-    Scanner myObj = new Scanner(System.in);
+    Scanner a1 = new Scanner(System.in);
 
     System.out.println("Enter name, age and salary:");
 
-    String name = myObj.nextLine();
-    int age =myObj.nextInt();
-    int salary=myObj.nextInt();
+    String name = a1.nextLine();
+    int age =a1.nextInt();
+    int salary=a1.nextInt();
     System.out.println("Name: " + name);
     System.out.println("Age: " + age);
     System.out.println("Salary: " + salary);
